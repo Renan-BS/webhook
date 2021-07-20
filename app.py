a80a25a28b3e61e740e7e166375e5ebb53a0475b
Renan-BS <87722279+Renan-BS@users.noreply.github.com>
2021-07-20 18:13:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, Response, json, jsonify
+from flask import Flask, request, Response, json
+import httpx
 
 app = Flask(__name__)
 
@@ -8,9 +9,23 @@ def api_root():
 
 @app.route('/webhook', methods=['POST'])
 def respond():
-    request_data = request.get_json()
-    return "{}".format(request_data)
+    dados = json.dumps(request.json)
+    id = getIdFromEmailZendesk(dados['previous']['email'][0]['value'])
+    base_url = 'https://bsinvestimentos.zendesk.com/'
+    if id is not None:
+        r = httpx.put(base_url+f'/api/v2/users/{id}', auth=('gustavo.garcia@bsinvestimentos.com.br', 'bs@2021'), data=json.dumps({'corretagem_ultimo_mes':'teste'}), headers={"Content-Type": "application/json"})
+        #return Response(status=200)
 
-    
+def getIdFromEmailZendesk(email):
+    base_url = 'https://bsinvestimentos.zendesk.com/'
+    headers = {"Content-Type": "application/json"}
+    params = {'role': 'end-user'}
+    r = httpx.get(base_url + f'/api/v2/search?query=email:{email}', auth=('gustavo.garcia@bsinvestimentos.com.br', 'bs@2021'), headers=headers, params=params, timeout=None)
+    id = r.json()['results'][0]['id']
+    if id is None:
+        return None
+    else: 
+        return id
+   
 if __name__=='__main__':
     app.run()
