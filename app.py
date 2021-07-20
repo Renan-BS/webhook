7c8452e3c2a55da27024719f02eeeb908160e129
Renan-BS <87722279+Renan-BS@users.noreply.github.com>
2021-07-20 19:29:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def trataTelefone(dado, campo):
 
 def MontaDicionario(dado):
     dicio = {"user":{}}
-    dicio['user']['phone'] = trataTelefone(dado, '657af87f9622875cde313deb4d10ad274ca6aa04'),
+    dicio['user']['phone'] = trataTelefone(dado, '657af87f9622875cde313deb4d10ad274ca6aa04')
     return json.dumps(dicio)
     
 
