d8f1c03f63ea5d45ef5f0a88374a1ad8c21d5d2e
Renan-BS <87722279+Renan-BS@users.noreply.github.com>
2021-07-20 16:22:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 def api_root():
     return 'Tá funcionando!'
 
-@app.route('/webhook', methods=['POST'])
+@app.route('/webhook', methods=['POST',])
 def respond():
     print(request.json)
     if request.headers['Content-Type'] == 'application/json':
-        return json.dumps(request.json)
-        #return Response(status=200)
+        #return json.dumps(request.json)
+        return Response(status=200)
     
 if __name__=='__main__':
     app.run()
