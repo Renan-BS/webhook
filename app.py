61db355d5b9e55685004bf341dd879783b87d5ed
renan-bs <renan@bsinvestimentos.com.br>
2021-08-11 16:13:49 -0300

    teste

--- app.py
+++ app.py
@@ -65,17 +65,19 @@ contas_duplicadas = {
 def update(self, id, dados):
     dicio = MontaDicionario(dados['current'])
     base_url = 'https://bsinvestimentos.zendesk.com/'
+    print(id, dados)
     try:
         r = httpx.put(base_url+f'/api/v2/users/{id}', auth=('gustavo.garcia@bsinvestimentos.com.br', 'blu3st4r'), data=dicio, headers={"Content-Type": "application/json"}, timeout=None)    
+        if r.status_code == 200:
+            print(id, 'Deu certo Porra')
+        else:
+            print('Erro de código', r.status_code)
+        r.close()
+        return Response(status=200)
     except:
         self.retry()
         
-    if r.status_code == 200:
-        print(id, 'Deu certo Porra')
-    else:
-        print('Erro de código', r.status_code)
-    r.close()
-    return Response(status=200)
+    
 
 @flask_app.route('/')
 def api_root():
