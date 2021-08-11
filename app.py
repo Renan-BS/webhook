e9f8f9972e1a8fc9ef7b143dfd4b5a37769e68ce
renan-bs <renan@bsinvestimentos.com.br>
2021-08-11 16:04:10 -0300

    teste

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def respond():
     id = getIdFromEmailZendesk(dados['previous']['email'][0]['value'])
     if id is not None:
         time.sleep(1)
-        update.delay(id, dados)
+        update.delay(id=id, dados=dados)
         return Response(status=200)
     else: 
         return Response(status=200)
